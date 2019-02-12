1d4feeb64d975b92ba4c1ce767177fe9dff71fae
Kamal Uddin Panhwar <kamalpanhwar@gmail.com>
2019-02-12 22:15:20 +0500

    Added MCC and MCCI in country from database

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'rails', '~> 5.1.6', '>= 5.1.6.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.3.11'
 # Use Puma as the app server
-
+gem 'pry'
 
 gem 'puma', '~> 3.7'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/controllers/api/v1/phones_controller.rb
+++ app/controllers/api/v1/phones_controller.rb
@@ -3,7 +3,27 @@ class Api::V1::PhonesController < ApplicationController
 
   def show
     phone_object = TelephoneNumber.parse(params[:id])
-    render json: phone_object 
+    phnumber = {}
+
+    phnumber['is_valid'] = phone_object.valid?
+    phnumber['type'] = phone_object.valid_types
+
+    phnumber['nation_number'] = phone_object.national_number
+    phnumber['international_number'] = phone_object.international_number
+    phnumber['e164'] = phone_object.e164_number
+    phnumber['code'] = phone_object.country.country_code
+    phnumber['country_id'] = phone_object.country.country_id
+    phnumber['normalized_number'] = phone_object.parser.normalized_number
+    phnumber['timezone'] = phone_object.timezone
+
+    country = Country.where(:code => phnumber['code']).first
+    phnumber['mcc'] = country['mcc']
+
+    phnumber['mcci'] = country['mcci']
+    p country
+
+
+    render json: phnumber 
 
   end
 
