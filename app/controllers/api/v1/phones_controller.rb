d278e9c8604d2d49f98541d6c0d8eb655724bb72
Kamal Uddin Panhwar <kamalpanhwar@gmail.com>
2019-02-14 17:58:29 +0500

    Added mcn and mcc_mcn fields in out put of numbers

--- app/controllers/api/v1/phones_controller.rb
+++ app/controllers/api/v1/phones_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::PhonesController < ApplicationController
     phone_object = TelephoneNumber.parse(params[:id])
     phnumber = {}
 
+    p phone_object
     phnumber['is_valid'] = phone_object.valid?
     phnumber['type'] = phone_object.valid_types
-
     phnumber['nation_number'] = phone_object.national_number
     phnumber['international_number'] = phone_object.international_number
     phnumber['e164'] = phone_object.e164_number
@@ -15,10 +15,20 @@ class Api::V1::PhonesController < ApplicationController
     phnumber['country_id'] = phone_object.country.country_id
     phnumber['normalized_number'] = phone_object.parser.normalized_number
     phnumber['timezone'] = phone_object.timezone
-
-    country = Country.where(:code => phnumber['code']).first
+    country = Country.where(code: phnumber['code']).first
     phnumber['mcc'] = country['mcc']
     phnumber['mcci'] = country['mcci']
+
+    prefixes = City.where(Country_id: country).all
+
+    prefixes.each do |n|
+      if phnumber['normalized_number'].start_with?(n.area_code.to_s)
+        phnumber['mcn'] = n.mnc
+        phnumber['mcc_mnc'] = "#{phnumber['mcc']}#{n.mnc}"
+      end
+    end
+
+    
     render json: phnumber 
 
   end
