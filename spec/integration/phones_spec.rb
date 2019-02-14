fd777c64c26f9c77c903e45bdc396a8037a02453
Kamal Uddin Panhwar <kamalpanhwar@gmail.com>
2019-02-14 18:02:51 +0500

    Added mcn and mcc_mcn fields in out put of numbers

--- spec/integration/phones_spec.rb
+++ spec/integration/phones_spec.rb
@@ -1,21 +1,21 @@
 # spec/integration/phones_spec.rb
 require 'swagger_helper'
 
-describe 'Phone Number checking API' do
+describe 'Get details of phone number and MCC and MCN codes' do
 
-  path '/api/v1/checknumber/{id}' do
+  path '/api/v1/checknumber/{phone_number}' do
 
     get 'Retrieve phone number' do
       tags 'Phones'
       produces 'application/json'
-      parameter name: :id, :in => :path, :type => :string
+      parameter name: :phone_number, :in => :path, :type => :string
 
     response '200', 'number found' do
       schema type: :object,
         properties: {
           id: { type: :integer }
       },
-      required: [ 'id', 'name', 'status' ]
+      required: [ 'phone number', 'name', 'status' ]
       run_test!
     end
 
